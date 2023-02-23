edf3d3a46611eaa7bbae649ac75ced88248ef428
tracer-ics2022 <tracer@njuics.org>
2023-02-23 18:32:03 +0800

    >  compile NEMU 211220000 张三 Linux hunter 5.15.0-58-generic #64-Ubuntu SMP Thu Jan 5 11:43:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:32:03 up  2:59,  3 users,  load average: 0.34, 0.42, 0.35

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -77,7 +77,7 @@ static int cmd_si(char *args) {
 }  
 //dtyy addfuntion
 static int cmd_info(char *args) {
-    if(*args == 'r' && *(args + 1) == 0) isa_reg_display();
+    if(args != 0 && *args == 'r' && *(args + 1) == 0) isa_reg_display();
 	else if(args == 0) printf("argument fault! Please enter the argument!");
     else printf("argument fault! Please enter the right argument!\n");
     return 0;
