a9ee8a3ab63c7fef85506710041a4319d854b77d
tracer-ics2022 <tracer@njuics.org>
2023-02-26 21:36:15 +0800

    >  compile NEMU 211220000 张三 Linux hunter 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:36:15 up 10:29,  3 users,  load average: 0.69, 0.49, 0.38

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -161,8 +161,15 @@ int cal(Token* ex, int r)
 	for(i = 0; i < r; i ++, j ++)
 		if(ex[i].type != -1)
 		{
-			stack[j].type = ex[i].type;
-			stack[j].str = ex[i].type;
+			if(stack[i].type == TK_NUM)
+				stack[j].str = ex[i].type;
+			else
+			{
+				stack[j].type = ex[i].type;
+				stack[j].str = ex[i].type;
+				if(stack[i].type != TK_NOTYPE)
+				printf("%c", (char)stack[i].type);
+			}
 		}
 	for(i = 0; i < j; i ++)
 	{
