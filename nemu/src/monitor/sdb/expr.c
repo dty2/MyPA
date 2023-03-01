b0aaf1eece942a67b68cbd77940b768a5c12dc5b
tracer-ics2022 <tracer@njuics.org>
2023-03-01 14:01:37 +0800

    >  compile NEMU 211220000 张三 Linux hunter 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:01:37 up  1:54,  3 users,  load average: 0.27, 0.50, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -144,7 +144,7 @@ static bool make_token(char *e) {
 				for(; i < substr_len; i ++)	
 					sum += (int)(*(substr_start + i) - '0') * pow(16, substr_len - 3); 
 				sum = (int)sum;
-				tokens[nr_token ++].str = sum;
+				tokens[nr_token].str = sum;
 			    tokens[nr_token ++].type = TK_NUM;
 				break;
 			case '$': 
