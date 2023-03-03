ef9fb7caa44462dbdf03c627e3f78d7aabd55922
dtyy <284050500@qq.com>
2023-03-03 09:51:57 +0800

    meet a problem

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -18,7 +18,6 @@
 #include <cpu/difftest.h>
 #include <locale.h>
 
-void check();
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
