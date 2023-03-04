d9c795145efc4106cdfd06da018cede069e72ab2
tracer-ics2022 <tracer@njuics.org>
2023-03-04 18:49:52 +0800

    >  compile NEMU 211220000 张三 Linux hunter 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:49:52 up  4:47,  3 users,  load average: 0.38, 0.39, 0.31

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -36,8 +36,7 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
 	int k = 0;
-	int str_len = strlen(s); 
-	printf("%d\n", str_len);
+	//int str_len = strlen(s); 
 	for(; k < ARRLEN(regs); k ++)
 	{
 		if(!strcmp(regs[k], s))
@@ -45,6 +44,5 @@ word_t isa_reg_str2val(const char *s, bool *success) {
 			return gpr(k);
 		}
 	}
-	printf("%d\n", str_len);
   return 0;
 }
