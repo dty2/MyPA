2e17582f6833eed4e3ab7c094ee934f0e54c439f
tracer-ics2022 <tracer@njuics.org>
2023-04-23 17:46:03 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:46:03 up  8:38,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -42,10 +42,9 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	int *p = s;
 	for(int i = 0; i < n; i ++)
-		*(p + i) = c;
-	return p;
+		*((int *)s + i) = c;
+	return s;
   //panic("Not implemented");
 }
 
