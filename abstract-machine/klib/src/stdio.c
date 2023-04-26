3b5bb788a274812ce9ddd82cec4b3381aed38849
tracer-ics2022 <tracer@njuics.org>
2023-04-26 15:03:09 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:03:09 up 56 min,  0 users,  load average: 0.00, 0.01, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,30 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	va_list valist;
+	va_start(valist, fmt);
+	int i = 0;
+	for(; *(fmt + i) != '\0'; i ++)
+	{
+		if(*(fmt + i) == '%')
+		{
+			switch(*(fmt + i + 1))
+			{
+				case 'd':
+					*(out + i) = (char)(va_arg(valist, int) + '0');
+					i ++;
+				case 's':
+					char *p = va_arg(valist, char *);
+					for(int j = 0; *(p + j) != '\0'; j ++, i ++)
+						*(out + i) = *(p + j);
+				default :;
+			}
+		}
+		else *(out + i) = *(fmt + i);
+	}
+	va_end(valist);
+	return i;
+  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
