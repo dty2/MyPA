91088b6194299df6251f1013a1023c8a7e88886b
tracer-ics2022 <tracer@njuics.org>
2023-04-30 18:47:08 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:47:08 up 10:14,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,7 +23,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 10
+#define MAX_INST_TO_PRINT 50
 
 CPU_state cpu = {}; // said by dtyy: cpu in isa/riscv32/include/isa-def.h
 uint64_t g_nr_guest_inst = 0;
@@ -41,7 +41,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
   //add code
+#ifdef CONFIG_WATCHPOINT
   check();
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
