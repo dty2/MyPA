b7b24df72af9191ee258db253632ccfc6eb870b7
tracer-ics2022 <tracer@njuics.org>
2023-05-01 19:13:10 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:13:10 up 8 min,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,7 +23,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INST_TO_PRINT 50
+#define MAX_INST_TO_PRINT 300
 
 CPU_state cpu = {}; // said by dtyy: cpu in isa/riscv32/include/isa-def.h
 uint64_t g_nr_guest_inst = 0;
