29becbf266fdeeff90d34b6e0696c73dbbe844cf
tracer-ics2022 <tracer@njuics.org>
2023-05-26 00:26:05 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:26:05 up 13:14,  0 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -90,10 +90,10 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
 	  //add code
-      case 1: elf = optarg; break;
+      case 2: elf = optarg; break;
 	  //add end
       case 'd': diff_so_file = optarg; break;
-      case 2: img_file = optarg; return 0;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
