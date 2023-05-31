959a1a9bd3cfdb4447c2a483edaa0f6acbbb35af
tracer-ics2022 <tracer@njuics.org>
2023-06-01 00:29:14 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:29:14 up  5:04,  0 users,  load average: 0.04, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -87,7 +87,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 	iput(_this->logbuf);
 
 	//add code for ftrace
-	printf("%s\n", _this->logbuf + 23);
+	printf("%s\n", _this->logbuf + 24);
   
 }
 
