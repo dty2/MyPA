90e82c8a702419d88372db4efc25f28557a49c7f
tracer-ics2022 <tracer@njuics.org>
2023-06-01 23:17:17 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:17:17 up  2:21,  0 users,  load average: 0.06, 0.05, 0.01

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -162,7 +162,7 @@ void handleelf(FILE *file)
 	int num_fun = 0;
 	for(int i = 0; i < numofsym; i ++)
 	{
-			printf("%d ", STT_FUNC);
+			printf("%d ", Symtab_header->st_info);
 		if(Symtab_header->st_info == STT_FUNC)
 		{
 			printf("\n abd \n");
