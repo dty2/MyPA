1403985bf41eb8407334348b36f68de22d7ef5a1
tracer-ics2022 <tracer@njuics.org>
2023-06-02 20:01:07 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:01:07 up 11:44,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -83,7 +83,7 @@ void sim_t::diff_memcpy(reg_t dest, void* src, size_t n) {
 
 extern "C" {
 
-void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     s->diff_memcpy(addr, buf, n);
   } else {
@@ -91,7 +91,7 @@ void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   }
 }
 
-void difftest_regcpy(void* dut, bool direction) {
+__EXPORT void difftest_regcpy(void* dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     s->diff_set_regs(dut);
   } else {
@@ -99,34 +99,18 @@ void difftest_regcpy(void* dut, bool direction) {
   }
 }
 
-void difftest_exec(uint64_t n) {
+__EXPOTR void difftest_exec(uint64_t n) {
   s->diff_step(n);
 }
 
-void difftest_init(int port) {
+__EXPORT void difftest_init(int port) {
   difftest_htif_args.push_back("");
-  cfg_t cfg(/*default_initrd_bounds=*/std::make_pair((reg_t)0, (reg_t)0),
-		  /*default_bootargs=*/nullptr,
-		  /*default_isa=*/DEFAULT_ISA,
-		  /*default_priv=*/DEFAULT_PRIV,
-		  /*default_varch=*/DEFAULT_VARCH,
-		  /*default_misaligned=*/false,
-		  /*default_endianness*/endianness_little,
-		  /*default_pmpregions=*/16,
-		  /*default_mem_layout=*/std::vector<mem_cfg_t>(),
-		  /*default_hartids=*/std::vector<size_t>(1),
-		  /*default_real_time_clint=*/false,
-		  /*default_trigger_count=*/4);
-  s = new sim_t(&cfg, false,
-	  difftest_mem, difftest_plugin_devices, difftest_htif_args,
-	  difftest_dm_config, nullptr, false, NULL,
-	  false,
-	  NULL,
-	  true);
+  cfg_t cfg(/*default_initrd_bounds=*/std::make_pair((reg_t)0, (reg_t)0),/*default_bootargs=*/nullptr,/*default_isa=*/DEFAULT_ISA,/*default_priv=*/DEFAULT_PRIV,/*default_varch=*/DEFAULT_VARCH,/*default_misaligned=*/false,/*default_endianness*/endianness_little,/*default_pmpregions=*/16,/*default_mem_layout=*/std::vector<mem_cfg_t>(),/*default_hartids=*/std::vector<size_t>(1),/*default_real_time_clint=*/false,/*default_trigger_count=*/4);
+  s = new sim_t(&cfg, false,difftest_mem, difftest_plugin_devices, difftest_htif_args,difftest_dm_config, nullptr, false, NULL,false,NULL,true);
 	s->diff_init(port);
 }
 
-void difftest_raise_intr(uint64_t NO) {
+__EXPORT void difftest_raise_intr(uint64_t NO) {
   trap_t t(NO);
   p->take_trap_public(t, state->pc);
 }
