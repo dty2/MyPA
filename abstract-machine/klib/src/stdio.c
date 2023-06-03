89e4c0c013f7c078dac6e480d161e94096501a75
tracer-ics2022 <tracer@njuics.org>
2023-06-03 22:00:38 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:00:38 up  7:39,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -22,7 +22,43 @@ void itoa_ten(int number, char *str)
 }
 
 int printf(const char *fmt, ...) {
-	panic("Not implemented");
+	va_list valist;
+	va_start(valist, fmt);
+	int i = 0, sum = 0;
+	char out[100000];
+	for(int i = 0; *(out + i); i ++)
+		*(out + i) = '\0';
+	for(; *(fmt + i) != '\0'; i ++)
+	{
+		if(*(fmt + i) == '%')
+		{
+			switch(*(fmt + i + 1))
+			{
+				case 'd':
+					char number[100];
+					itoa_ten(va_arg(valist, int), number);
+					strcat(out, number);
+					while(*(out + sum)) sum ++;
+					break;
+				case 's':
+					char *p = va_arg(valist, char *);
+					strcat(out, p);
+					while(*(out + sum)) sum ++;
+					break;
+			}
+			i ++;
+		}
+		else
+		{
+			*(out + sum) = *(fmt + i);
+			sum ++;
+		}
+	}
+	*(out + sum + 1) = '\0';
+	va_end(valist);
+	putstr(out);
+	return sum;
+	//panic("Not implemented");
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
