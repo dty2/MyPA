904af87b7a9f777ef0701724da45125482ee8a2d
tracer-ics2022 <tracer@njuics.org>
2023-06-06 12:13:42 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:13:42 up  2:15,  0 users,  load average: 0.37, 0.26, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  printf("hello %d hello\n", inl(RTC_ADDR));
+  printf("hello %x hello\n", inl(RTC_ADDR));
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
