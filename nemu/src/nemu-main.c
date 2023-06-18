ce0d2d68cc3013eed6b89241cd1f248dbbe2283a
tracer-ics2022 <tracer@njuics.org>
2023-06-18 18:31:23 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:31:23 up  3:12,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
+  printf("\nhello\n");
 #else
   init_monitor(argc, argv);
 #endif
