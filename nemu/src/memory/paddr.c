1227cd360a4bece32dde27ffe6eabc2e0aabee43
tracer-ics2022 <tracer@njuics.org>
2023-06-19 12:54:03 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:54:03 up  3:41,  0 users,  load average: 0.03, 0.06, 0.09

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -75,7 +75,7 @@ word_t paddr_read(paddr_t addr, int len) {
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return;);
   geterr();
   out_of_bound(addr);
 }
