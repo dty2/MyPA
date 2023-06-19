0bf683fe3f7405403000355d0ed311c7dd71c63b
tracer-ics2022 <tracer@njuics.org>
2023-06-19 13:42:46 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:42:46 up  4:30,  0 users,  load average: 0.06, 0.04, 0.00

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -41,6 +41,7 @@ static void timer_intr() {
 void init_timer() {
   rtc_port_base = (uint32_t *)new_space(8);
 #ifdef CONFIG_HAS_PORT_IO
+  printf("hello");
   add_pio_map ("rtc", CONFIG_RTC_PORT, rtc_port_base, 8, rtc_io_handler);
 #else
   printf("hello");
