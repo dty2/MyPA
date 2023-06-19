19010d9ca1e913ab3d3307c3369553c518494010
tracer-ics2022 <tracer@njuics.org>
2023-06-19 12:47:07 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:47:07 up  3:34,  0 users,  load average: 0.21, 0.11, 0.14

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,7 +8,7 @@ uint64_t gettime()
 {
 	uint64_t time1 = (uint64_t)inl(AUDIO_ADDR);
 	uint64_t time2 = (uint64_t)inl(AUDIO_ADDR + 4) << 32;
-	printf("%d ", time2 | time1);
+	printf("%ld ", time2 | time1);
 	return time2 | time1;
 }
 
