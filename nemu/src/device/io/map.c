804c479e5133ba723ef23716c9125872c94662ba
tracer-ics2022 <tracer@njuics.org>
2023-06-19 13:00:23 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:00:23 up  3:48,  0 users,  load average: 0.02, 0.07, 0.08

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -44,6 +44,7 @@ static void check_bound(IOMap *map, paddr_t addr) {
 
 static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write) {
   if (c != NULL) { c(offset, len, is_write); }
+  else printf("c is NULL");
 }
 
 void init_map() {
