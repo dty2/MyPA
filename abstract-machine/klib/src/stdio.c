7049d2268acaa52617be1725ad4fb22ad521eb82
tracer-ics2022 <tracer@njuics.org>
2023-06-20 15:43:37 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:43:37 up  1:19,  0 users,  load average: 0.06, 0.05, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+/*
 void itoa_ten(int number, char *str)
 {
 	int i = 0;
@@ -20,7 +21,66 @@ void itoa_ten(int number, char *str)
 	}
 	*(str + i) = '\0';
 }
+*/
+char* itoa(int num, char* buf)
+{
+  int i = 0;
+  int len = 0;
+  int flag = 0;
+  char tmp;
+  if (num == 0) {
+    buf[i++] = '0';
+  }
+  if (num < 0) {
+    num *= -1;
+    flag = 1;
+  }
+  while (num) {
+    buf[i++] = num % 10 + '0';
+    num = num /10;
+  }
+  if (flag) {
+    buf[i++] = '-';
+  }
+  buf[i] = '\0';
+  len = i;
+  for (i = 0; i < len / 2; i++) {
+    tmp = buf[i];
+    buf[i] = buf[len-i-1];
+    buf[len-i-1] = tmp;
+  }
+  return buf;
+}
 
+char* xtoa(unsigned int num, char* buf)
+{
+  int i = 0;
+  if (num == 0) {
+    buf[i++] = '0';
+  }
+  while (num) {
+    if (num % 16 < 10) {
+      buf[i++] = num % 16 + '0';
+    }
+    else {
+      buf[i++] = num % 16 - 10 + 'A';
+    }
+    num = num / 16;
+  }
+  buf[i++] = 'x';
+  buf[i++] = '0';
+  buf[i] = '\0';
+  int len = i;
+
+  char tmp;
+  for (i = 0; i < len / 2; i++) {
+    tmp = buf[i];
+    buf[i] = buf[len-i-1];
+    buf[len-i-1] = tmp;
+  }
+  return buf;
+}
+/*
 int printf(const char *fmt, ...) {
 	va_list valist;
 	va_start(valist, fmt);
@@ -60,11 +120,63 @@ int printf(const char *fmt, ...) {
 	return sum;
 	//panic("Not implemented");
 }
-
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
+*/
 
+int vsprintf(char *out, const char *fmt, va_list vl) {
+  int count = 0;
+  int i = 0;
+  while (*fmt != '\0') {
+    if (*fmt != '%') {
+      out[i++] = *fmt++;
+      count++;
+      //puts("hit");
+    }
+    else {
+      ++fmt;
+      if (*fmt == 's') {
+        char* src = va_arg(vl, char*);
+        while (*src != '\0') {
+          out[i++] = *src++;
+          count++;
+        }
+        ++fmt;
+        //puts("shit hit s");
+      }
+      else if (*fmt == 'd') {
+        char buf[33];
+        itoa(va_arg(vl, int), buf);
+        int index = 0;
+        while (buf[index] != '\0') {
+          out[i++] = buf[index++];
+          count++;
+        }
+        ++fmt;
+        //puts("shit hit d");
+      }
+      else if (*fmt == 'p') {
+        char buf[33];
+        xtoa(va_arg(vl, int), buf);
+        int index = 0;
+        while (buf[index] != '\0') {
+          out[i++] = buf[index++];
+          count++;
+        }
+        ++fmt;
+      }
+      else {
+        out[i++] = '%';
+        count += 1;
+      }
+    }
+  }
+  out[i] = '\0';
+  if (count == 0) return -1;
+  return count;
+}
+/*
 int sprintf(char *out, const char *fmt, ...) {
 	va_list valist;
 	va_start(valist, fmt);
@@ -101,6 +213,26 @@ int sprintf(char *out, const char *fmt, ...) {
 	va_end(valist);
 	return sum;
   //panic("Not implemented");
+}
+*/
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list vl;
+  va_start(vl, fmt);
+  int count = vsprintf(out, fmt, vl);
+  va_end(vl);
+  if (count == 0) return -1;
+  return count;
+}
+
+int printf(const char *fmt, ...) {
+  char buf[2048];
+  va_list vl_src;
+  va_start(vl_src, fmt);
+  int count = vsprintf(buf, fmt, vl_src);
+  putstr(buf);
+  va_end(vl_src);
+  return count;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
