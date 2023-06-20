361da50c8ec8d0d3837c8219ff1b8bedb8231cc9
tracer-ics2022 <tracer@njuics.org>
2023-06-20 14:51:30 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:51:30 up 26 min,  0 users,  load average: 0.01, 0.00, 0.00

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -41,6 +41,5 @@ static uint64_t get_time_internal() {
 uint64_t get_time() {
   if (boot_time == 0) boot_time = get_time_internal();
   uint64_t now = get_time_internal();
-  printf("\n%ld\n",now - boot_time);
   return now - boot_time;
 }
