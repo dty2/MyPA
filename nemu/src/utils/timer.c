90861cb9ae4e10ad362d6c988d4ce61f7ee48463
tracer-ics2022 <tracer@njuics.org>
2023-06-20 14:10:54 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:10:54 up  3:01,  0 users,  load average: 0.06, 0.02, 0.00

--- nemu/src/utils/timer.c
+++ nemu/src/utils/timer.c
@@ -30,6 +30,7 @@ static uint64_t get_time_internal() {
   struct timeval now;
   gettimeofday(&now, NULL);
   uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  printf("\n%ld\n",now.tv_sec);
 #else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
