cf356371c3e45431c1ac715d04776710971ffaa9
tracer-ics2022 <tracer@njuics.org>
2023-06-20 15:53:28 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:53:28 up  1:28,  0 users,  load average: 0.00, 0.02, 0.00

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,11 +9,20 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
+  char *p = NULL;
+  if(dst == NULL || src == NULL) {
+      return NULL;
+  }
+  p = dst;
+  while((*dst++ = *src++) != '\0');
+  return p;
+	/*
 	int i = 0;
 	for(; *(src + i) != '\0'; i ++)
 		*(dst + i) = *(src + i);
 	*(dst + i) = '\0';
 	return dst;
+	*/
   //panic("Not implemented");
 }
 
@@ -42,11 +51,27 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
+  while ((*s1) && (*s1 == *s2)) {
+		s1++;
+		s2++;
+	}
+
+	if (*(unsigned char*)s1 > *(unsigned char*)s2) {
+		return 1;
+	}
+	else if (*(unsigned char*)s1 < *(unsigned char*)s2) {
+		return -1;
+	}
+	else {
+		return 0;
+	}
+	/*
 	int i = 0;
 	while(*(s1 + i) && *(s1 + i) == *(s2 + i)) i ++;
 	if(*(s1 + i) > *(s2 + i)) return 1;
 	else if(*(s1 + i) < *(s2 + i)) return -1;
 	else return 0;
+	*/
   //panic("Not implemented");
 }
 
@@ -55,9 +80,19 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
+  if (s == NULL || n < 0) {
+		return NULL;
+	}
+	char *pdest = (char *)s;
+	while (n-- > 0) {
+		*pdest++ = c;
+	}
+	return s;
+	/*
 	for(int i = 0; i < n; i ++)
 		*((char *)s + i) = c;
 	return s;
+	*/
   //panic("Not implemented");
 }
 
@@ -66,16 +101,34 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  if ((in == NULL) || (out == NULL)) return 0;
+  while (n--) {
+    *(char*)out++ = *(char*)in++;
+  }
+  return out;
+  //panic("Not implemented");
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
+  assert(s1 != NULL && s2 != NULL && n > 0);
+  const char *pdest = (char*)s1;
+  const char *psrc = (char*)s2;
+  while(*pdest == *psrc && --n > 0) {
+    pdest++;
+    psrc++;
+  }
+  int a = *pdest - *psrc;
+  if(a > 0) { return 1; }
+  else if(a < 0) { return -1; }
+	/*
 	int i = 0;
 	while(*((char *)s1 + i) == *((char *)s2 + i) && i < n) i ++;
 	if((*((char *)s1 + i)) > (*((char *)s2 + i)) && i != n) return 1;
 	else if(*((char *)s1 + i) < (*((char *)s2 + i)) && i != n) return -1;
 	else return 0;
+	*/
   //panic("Not implemented");
+  return 0;
 }
 
 #endif
