96082edc639cf5dbe2ffd4626276eb8a9b789be2
tracer-ics2022 <tracer@njuics.org>
2023-07-08 01:43:42 +0800

    >  compile NEMU 211220000 张三 Linux LAPTOP-MR760PBG 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:43:42 up  1:27,  0 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,23 +1,15 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
 #define CONFIG_I8042_DATA_MMIO 0xa0000060
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
   uint32_t state = *(uint32_t *)(CONFIG_I8042_DATA_MMIO);
-  // decode出keycode
   kbd->keycode = state & ~KEYDOWN_MASK;
+  printf("%d",state);
   if ((state & KEYDOWN_MASK) != 0) {
     kbd->keydown = true;
   }
 }
-/*
-void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
-
-}
-*/
